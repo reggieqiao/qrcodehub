c9a25fc5e578e6f207f89a1847b1140d464cc3b9
reggieqiao <reggieqiao@gmail.com>
2024-01-05 14:11:10 +0800

    feat:manifest version 3

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,10 @@
-chrome.tabs.getSelected(null, function(tab){
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var qrcode = document.getElementById('qrcode');
     var qr = new QRCodeStyling({
         width: 300,
         height: 300,
-        data: tab.url,
+        data: tabs[0].url,
         margin: 10
     });
     qr.append(qrcode);
 });
-  
